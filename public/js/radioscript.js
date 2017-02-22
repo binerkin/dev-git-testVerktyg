cd35b48a2a4b221eede84b4a03c5f0b78f82f987
Matilda Thorander <matilda.thorander@gmail.com>
2017-02-22 13:36:44 +0100

    Update

--- public/js/radioscript.js
+++ public/js/radioscript.js
@@ -2,22 +2,60 @@
  * Created by Matilda on 2017-02-22.
  */
 
+console.log("Hej");
 
+/**
+var radioButton = document.getElementById("#yesno");
+var getBody = document.getElementById("#choiceresult");
+
+$(document).ready(function(){
+    $("yesno").click(function(){
+        $("choiceresult").hide(1000);
+    });
+});
+**/
+
+$(document).ready(function() {
+    // show the table as soon as the DOM is ready
+    $("#choice1").show();
+    $("#choice2").hide();
+    $("#choice3").hide();
+
+    // hides the table on clicking the noted link
+    $("#radio1").click(function() {
+        $("#choice1").show();
+        $("#choice2").hide();
+        $("#choice3").hide();
+    });
+
+    // show the table on clicking the noted link
+    $("#radio2").click(function() {
+        $("#choice1").hide();
+        $("#choice2").show();
+        $("#choice3").hide();
+    });
+});
+
+
+/*
 $(function(){
+    console.log('test');
     // cache these!
-    var $radioButtons = $("yesno"); // get the radiobutton
-    var $section = $("choiceresult"); //get the body
+    var radioButton = document.getElementById("#yesno"); // get the radio button
+    var setBody = document.getElementById("#choiceresult"); //get the body
 
-    $radioButtons.create(function () {
-        if( $(this).val() === "yesno" ) { // check the value
+    radioButton.create(function() {
+        if( $(this).val() === "#yesno" ) { // check the value
             var x = document.createElement("INPUT");
             x.setAttribute("type", "radio");
-            $section.appendChild(x);
-            //document.body.appendChild(x); // .choiceresult
-            //section(x);
+            document.body.appendChild(setBody(x));
+            //setBody(x);
+            //document.body.appendChild(x);
         } else {
-            yesno.hide();
+            radioButton.hide();
         }
     });
 });
+
+*/
 
