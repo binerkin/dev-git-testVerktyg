56a98528a292ba0afab3ac3d61a0788b64c5a3b6
Matilda Thorander <matilda.thorander@gmail.com>
2017-02-23 13:48:39 +0100

    update

--- public/js/buttonscript.js
+++ public/js/buttonscript.js
@@ -1,29 +1,10 @@
 /**
  * Created by Matilda on 2017-02-23.
+ *
+ * Skapar flera formulärer
  */
 
 
-
-/**
-$(document).ready(function(){
-
-    $("#severalanswer").on('click', function(e){
-       e.preventDefault();
-       $("#test").hide();
-    });
-
-});
-**/
-/*
-function a() {
-    document.write("<form id='form1' name='form1'action=''>");
-    document.write(" <td><input name='name' type='text' id='name'/></td>");
-    document.write("<td><input name='address' type='text' id='address'/></td>");
-
-    document.write("");
-
-}
-*/
 
 var formItem = $("<input type ='text' placeholder='Annat svar'><input type='checkbox' name='correctanswer' class='correctanswer' value='correct'> Rätt svar</input>");
 
