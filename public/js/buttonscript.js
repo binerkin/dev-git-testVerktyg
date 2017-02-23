5b098f4b7e99bec93c6c5f6d8922965cf18fb610
Matilda Thorander <matilda.thorander@gmail.com>
2017-02-23 12:56:00 +0100

    update

--- public/js/buttonscript.js
+++ public/js/buttonscript.js
@@ -2,13 +2,38 @@
  * Created by Matilda on 2017-02-23.
  */
 
-console.log("Hej");
 
+
+/**
 $(document).ready(function(){
     $("#severalanswer").click(function(){
        $("#test").hide();
     });
 
 });
+**/
+/*
+function a() {
+    document.write("<form id='form1' name='form1'action=''>");
+    document.write(" <td><input name='name' type='text' id='name'/></td>");
+    document.write("<td><input name='address' type='text' id='address'/></td>");
+
+    document.write("");
+
+}
+*/
+
+var formItem = $("<input type ='text' placeholder='Annat svar'><input type='checkbox' name='correctanswer' class='correctanswer' value='correct'> Rätt svar</input>");
+
+$('document').ready(function(){
+   $('#addForm').on('click',function(e){
+       console.log('test');
+       e.preventDefault();
+       $('#formToBe').append(formItem);
+
+   });
+});
+
+
 
 
