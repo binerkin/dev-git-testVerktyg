3d888a4d868711df9826564f26af62773ee77fba
Matilda Thorander <matilda.thorander@gmail.com>
2017-02-23 10:40:06 +0100

    update

--- public/js/radioscript.js
+++ public/js/radioscript.js
@@ -1,19 +1,8 @@
 /**
  * Created by Matilda on 2017-02-22.
+ *
+ * Funktion till frågalternativ (radiobuttons).
  */
-
-console.log("Hej");
-
-/**
-var radioButton = document.getElementById("#yesno");
-var getBody = document.getElementById("#choiceresult");
-
-$(document).ready(function(){
-    $("yesno").click(function(){
-        $("choiceresult").hide(1000);
-    });
-});
-**/
 
 $(document).ready(function() {
     // show the table as soon as the DOM is ready
@@ -35,27 +24,3 @@ $(document).ready(function() {
         $("#choice3").hide();
     });
 });
-
-
-/*
-$(function(){
-    console.log('test');
-    // cache these!
-    var radioButton = document.getElementById("#yesno"); // get the radio button
-    var setBody = document.getElementById("#choiceresult"); //get the body
-
-    radioButton.create(function() {
-        if( $(this).val() === "#yesno" ) { // check the value
-            var x = document.createElement("INPUT");
-            x.setAttribute("type", "radio");
-            document.body.appendChild(setBody(x));
-            //setBody(x);
-            //document.body.appendChild(x);
-        } else {
-            radioButton.hide();
-        }
-    });
-});
-
-*/
-
