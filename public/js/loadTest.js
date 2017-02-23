a6214d6e76da579d9fd2d172d0c229dc6a8812b8
Johan <johan@MacBook-Pro.local>
2017-02-23 11:35:24 +0100

    Css för welcome page Fixa get Welcome script bättre Load test skript påbörjat

---
+++ public/js/loadTest.js
@@ -0,0 +1,9 @@
+/**
+ * Created by johan on 2017-02-23.
+ */
+$('document').ready(function(){
+
+    $('#container').on('click','a#startTest',function() {
+        console.log('startTest');
+    });
+});
